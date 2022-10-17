3de98f9d19fe10d0e80e44db6537efb970892a4c
Fabio Damian <fabio@soydigitalmind.com>
2022-10-17 20:32:54 -0300

    fechas y horas

---
+++ 1. Familiarizarse con las Funciones de Fechas y horas básicas de PHP/3-session-despues/end_session.php
@@ -0,0 +1,22 @@
+<?php
+
+$inactivity_limit = 5;
+
+session_start();
+
+if (! isset($_SESSION['authenticated'])) {
+    header('Location: login.php');
+    exit;
+} elseif ($_SESSION['authenticated'] + $inactivity_limit < time()) {
+    $_SESSION = [];
+
+    if (isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), time() - 86400, '/');
+    }
+
+    session_destroy();
+    header('Location: login.php?expired=true');
+    exit;
+} else {
+    $_SESSION['authenticated'] = time();
+}
